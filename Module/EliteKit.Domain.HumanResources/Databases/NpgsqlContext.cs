ab0f433dcbbd15dbb19991f4b1a7711c7bf7980a
Eli HU <yichen.major@gmail.com>
2024-12-16 11:30:03 +0800

    commit all

--- Module/EliteKit.Domain.HumanResources/Databases/NpgsqlContext.cs
+++ Module/EliteKit.Domain.HumanResources/Databases/NpgsqlContext.cs
@@ -1,13 +1,20 @@
+using System.Reflection;
 using EliteKit.Domain.HumanResources.Databases.Users;
 using Microsoft.EntityFrameworkCore;
 
 namespace EliteKit.Domain.HumanResources.Databases;
 public partial class NpgsqlContext(DbContextOptions<NpgsqlContext> options) : DbContext(options)
 {
-    public DbSet<UserRegistration> UserRegistrations { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        
+        
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-
+        //專案內有 IEntityTypeConfiguration<> 反射實作
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
+    public DbSet<UserRegistration> UserRegistrations { get; set; }
 }
--- Module/EliteKit.Domain.HumanResources/Databases/Users/UserRegistration.cs
+++ Module/EliteKit.Domain.HumanResources/Databases/Users/UserRegistration.cs
@@ -2,7 +2,10 @@
 
 public class UserRegistration
 {
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public int Age { get; set; }
+    public Guid Id { get; set; }
+    public string Account { get; set; }
+    public DateTime CreateTime { get; set; }
+    public string Email { get; set; }
+    
+    
 }
